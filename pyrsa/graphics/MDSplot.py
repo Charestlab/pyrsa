6e44b49738e3625830aa32d70ba5da6d99792004
Jasper van den Bosch <jasperb@uw.edu>
2014-04-04 18:25:37 -0700

    Basic MDS capabilities

---
+++ pyrsa/graphics/MDSplot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
+
+
+def MDSplot(similarities, pos):
+	fig = plt.figure(1)
+	ax = plt.axes([0., 0., 1., 1.])
+
+	plt.scatter(pos[:, 0], pos[:, 1], s=20, c='g')
+
+	similarities = similarities.max() / similarities * 100
+	similarities[np.isinf(similarities)] = 0
+
+#	# Plot the edges
+#	start_idx, end_idx = np.where(pos)
+#	#a sequence of (*line0*, *line1*, *line2*), where::
+#	#            linen = (x0, y0), (x1, y1), ... (xm, ym)
+#	segments = [[X_true[i, :], X_true[j, :]]
+#		    for i in range(len(pos)) for j in range(len(pos))]
+#	values = np.abs(similarities)
+#	lc = LineCollection(segments,
+#		            zorder=0, cmap=plt.cm.hot_r,
+#		            norm=plt.Normalize(0, values.max()))
+#	lc.set_array(similarities.flatten())
+#	lc.set_linewidths(0.5 * np.ones(len(segments)))
+#	ax.add_collection(lc)
+
+	plt.show()
---
+++ pyrsa/mds.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import numpy as np
+from sklearn.manifold import MDS
+from sklearn.metrics import euclidean_distances
+from sklearn.decomposition import PCA
+
+
+def mds(similarities):
+	seed = np.random.RandomState(seed=3)
+
+	mds = MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed,
+		           dissimilarity="precomputed", n_jobs=1)
+	pos = mds.fit(similarities).embedding_
+
+	#nmds = MDS(n_components=2, metric=False, max_iter=3000, eps=1e-12,
+	#                    dissimilarity="precomputed", random_state=seed, n_jobs=1,
+	#                    n_init=1)
+	#npos = nmds.fit_transform(similarities, init=pos)
+
+	# Rescale the data
+	#pos *= np.sqrt((X_true ** 2).sum()) / np.sqrt((pos ** 2).sum())
+	#npos *= np.sqrt((X_true ** 2).sum()) / np.sqrt((npos ** 2).sum())
+
+	# Rotate the data
+	clf = PCA(n_components=2)
+	pos = clf.fit_transform(pos)
+	#npos = clf.fit_transform(npos)
+	return pos
