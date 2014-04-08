aa0d7ef6703d1653eb0342fe7f446aee960053fb
Jasper van den Bosch <jasperb@uw.edu>
2014-04-08 15:04:00 -0700

    Y-ticklabels for RDM image

--- pyrsa/graphics/showRDM.py
+++ pyrsa/graphics/showRDM.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import matplotlib.pyplot as plt
+import numpy as np
 
 def showRDM(rdm, save=False):
     plt.matshow(rdm.square)
     plt.title(rdm.name)
+    plt.yticks(np.arange(len(rdm.labels)), rdm.labels, size=8)
+    plt.xticks(np.arange(len(rdm.labels)), rdm.labels, size=8, rotation=90)
     if save:
         plt.savefig(rdm.name.replace(' ','_')+'.png')
         plt.close()
