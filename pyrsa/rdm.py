741c19a5da805a48185c0bd684566eff0881c2d3
Jasper van den Bosch <jasperb@uw.edu>
2014-04-28 15:20:56 -0700

    pyrsa.rdm.secondorder(list_of_rdms) creates a second-order RDM

--- pyrsa/rdm.py
+++ pyrsa/rdm.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from scipy.spatial.distance import pdist, squareform
+import numpy
 
 def create(patterns):
     rdm = RDM()
     rdm.utv = pdist(patterns,'correlation')
     rdm.square = squareform(rdm.utv)
+    return rdm
+
+def secondorder(rdmlist):
+    utvs = numpy.array([r.utv for r in rdmlist])
+    rdm = RDM()
+    rdm.utv = pdist(utvs,'correlation')
+    rdm.square = squareform(rdm.utv)
+    rdm.labels = [r.name for r in rdmlist]
     return rdm
 
 
