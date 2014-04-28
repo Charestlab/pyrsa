13632c9b43e138fbf9e7a2e35a5d628cf33474a3
Jasper van den Bosch <jasperb@uw.edu>
2014-04-28 15:45:10 -0700

    MDS can now be scaled although this does not really work as expected.

--- pyrsa/graphics/MDSplot.py
+++ pyrsa/graphics/MDSplot.py
@@ -10,27 +10,12 @@ def MDSplot(rdm, pos, save=False):
     iconsAsLabels = True
     similarities = rdm.square
     fig = plt.figure(1)
-    ax = plt.axes([0., 0., 1., 1.])
+    ax = plt.axes([0., 0., 1., 1.]) #
 
-    plt.scatter(pos[:, 0], pos[:, 1], s=0)
+    
+    plt.scatter(pos[:, 0], pos[:, 1])
 
-    similarities = similarities.max() / similarities * 100
-    similarities[np.isinf(similarities)] = 0
-
-#    # Plot the edges
-#    start_idx, end_idx = np.where(pos)
-#    #a sequence of (*line0*, *line1*, *line2*), where::
-#    #            linen = (x0, y0), (x1, y1), ... (xm, ym)
-#    segments = [[X_true[i, :], X_true[j, :]]
-#            for i in range(len(pos)) for j in range(len(pos))]
-#    values = np.abs(similarities)
-#    lc = LineCollection(segments,
-#                    zorder=0, cmap=plt.cm.hot_r,
-#                    norm=plt.Normalize(0, values.max()))
-#    lc.set_array(similarities.flatten())
-#    lc.set_linewidths(0.5 * np.ones(len(segments)))
-#    ax.add_collection(lc)
-    if not iconsAsLabels:
+    if not hasattr(rdm, 'images'):
         for s in range(len(rdm.labels)):
             plt.annotate(rdm.labels[s], xy = (pos[s, 0], pos[s, 1]), xytext = (-20, 20),
                 textcoords = 'offset points')
@@ -44,3 +29,5 @@ def MDSplot(rdm, pos, save=False):
         plt.close()
     else:
         plt.show()
+
+
