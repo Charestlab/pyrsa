855e57bb7d36f07398ebfa152d4e8b86d75c47f8
Jasper van den Bosch <jasperb@uw.edu>
2014-10-27 16:32:11 -0700

    Improved RDM.ranktransformed

--- pyrsa/rdm.py
+++ pyrsa/rdm.py
@@ -25,19 +25,17 @@ class RDM(object):
 
     @property
     def ranktransformed(self):
-        ranks = numpy.arange(self.square.size)
-        orderedIndices = numpy.argsort(self.square, None)
-        rankVector = ranks[orderedIndices]
-        return rankVector.reshape(self.square.shape)
+        rankList, ranksFlat = numpy.unique(self.square, return_inverse=True)
+        maxRank = rankList.size-1
+        ranksFlat = ranksFlat/maxRank
+        return ranksFlat.reshape(self.square.shape)
 
 
-
+##### MATLAB
 #function rankArray=rankTransform(array,scale01)
 
 #% transforms the array 'array' by replacing each element by its rank in the
 #% distribution of all its elements
-
-
 
 #if ~exist('scale01','var'), scale01=false; end;
 
@@ -53,3 +51,32 @@ class RDM(object):
 #if scale01
 #    rankArray=rankArray/numel(set);
 #end
+
+##### Javascript
+#function rankTransform(RDM){
+#    /*
+#    transforms the matrix mat by replacing each element by its rank in the
+#    distribution of all its elements. if scale01 is set the ranked elements
+#    would be scaled between 0 to 1. One property of this function, as
+#    indicated by its name, is that equal values would stay equal after
+#    ranking and scaling. The way the function does this is by
+#    assigning the mean scaled rank to all the equal entries of the input
+#    matrix (mat). NaNs are ignored in this process.
+#    __________________________________________________________________________
+#    Copyright (C) 2014 Ian Charest @ Medical Research Council
+#    */
+#    // get the upper triangular vector
+#    var utv = triu(RDM);
+#    /*console.log(utv)*/
+
+#    var sorted = utv.slice().sort(function(a,b){return b-a})
+#    var rdmranks = utv.slice().map(function(v){ return sorted.indexOf(v)+1 });
+
+#    var maxRank = utv.length;
+
+#    for (var i =0; i < maxRank;i++){
+#        rdmranks[i] = 1 - rdmranks[i]/maxRank;
+#    }
+
+#    return squareform(rdmranks)
+#}
--- pyrsa/tests/test_RDM.py
+++ pyrsa/tests/test_RDM.py
@@ -16,11 +16,40 @@ class RDMTests(unittest.TestCase):
         from pyrsa import RDM
         rdm = RDM(dissims)
         rdmR = rdm.ranktransformed
-        #all integers
-        self.assertEqual(list(range(dissims.size)), 
-            numpy.sort(numpy.ravel(rdmR)).tolist())
+        # same dimensions
+        self.assertEqual(rdmR.shape, rdm.square.shape)
+        # rank preserved
+        self.assertGreater(rdmR[0,1], rdmR[0,0])
+        self.assertGreater(rdmR[0,3], rdmR[0,1])
+        # same values stay same
+        self.assertEqual(rdmR[0,1], rdmR[0,2])
+        # max 1 min 0
+        self.assertEqual(rdmR.max(), 1)
+        self.assertEqual(rdmR.min(), 0)
 
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+#dissims = numpy.array([ [0,   0.1, 0.1, 0.9, 0.9, 0.9], 
+#                    [0.1, 0,   0.1, 0.9, 0.9, 0.9], 
+#                    [0.1, 0.1, 0,   0.9, 0.9, 0.9], 
+#                    [0.9, 0.9, 0.9, 0,   0.1, 0.1], 
+#                    [0.9, 0.9, 0.9, 0.1, 0,   0.1], 
+#                    [0.9, 0.9, 0.9, 0.1, 0.1, 0  ], 
+#                    ])
+
+
+#In [6]: numpy.arange(36).reshape([6,6])
+#Out[6]: 
+#array([[ 0,  1,  2,  3,  4,  5],
+#       [ 6,  7,  8,  9, 10, 11],
+#       [12, 13, 14, 15, 16, 17],
+#       [18, 19, 20, 21, 22, 23],
+#       [24, 25, 26, 27, 28, 29],
+#       [30, 31, 32, 33, 34, 35]])
+
