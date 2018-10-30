9f030fd9c380ebbcf25535052fc1431f226856d6
Jasper <japsai@gmail.com>
2018-10-30 19:00:22 +0100

    rdm movie shows time stamps

--- pyrsa/animated.py
+++ pyrsa/animated.py
@@ -3,11 +3,13 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 
 def update(f, *fargs):
-    utvs, ax, title = fargs
+    utvs, ax, title, timepoints = fargs
     utv = utvs[f, :]
+    if timepoints is not None:
+        title = title + ' t={:.3f}'.format(timepoints[f])
     return plot_rdm(utv, title, ax=ax),
 
-def movie_rdm(utvs, title):
+def movie_rdm(utvs, title='RDM movie', timepoints=None):
     nframes, _ = utvs.shape
     fig, ax = plt.subplots()
     return FuncAnimation(
@@ -15,5 +17,5 @@ def movie_rdm(utvs, title):
         update,
         frames=nframes,
         repeat=False,
-        fargs=(utvs, ax, title)
+        fargs=(utvs, ax, title, timepoints)
     )
--- pyrsa/plotting.py
+++ pyrsa/plotting.py
@@ -6,4 +6,5 @@ def plot_rdm(utv, title, ax=None):
     rdm = scipy.spatial.distance.squareform(utv)
     if ax is None:
         fig, ax = plt.subplots()
+    ax.set_title(title)
     return ax.imshow(rdm)
