22ee75012d4ebc8a805d4d242fe38c3ed863cd2b
Jasper <japsai@gmail.com>
2019-02-19 22:41:30 +0000

    new mds_plot function

--- pyrsa/mds.py
+++ pyrsa/mds.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import numpy as np
+import numpy
+from numpy import sqrt
+import scipy.spatial.distance
 from sklearn.manifold import MDS
 from sklearn.metrics import euclidean_distances
 from sklearn.decomposition import PCA
 
 
-def mds(rdm):
-    seed = np.random.RandomState(seed=3)
-
+def mds(utv):
+    rdm = scipy.spatial.distance.squareform(utv)
+    seed = numpy.random.RandomState(seed=3)
     mds = MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed,
                    dissimilarity="precomputed", n_jobs=1)
-    pos = mds.fit(rdm.square).embedding_
+    pos = mds.fit(rdm).embedding_
 
-    #nmds = MDS(n_components=2, metric=False, max_iter=3000, eps=1e-12,
-    #                    dissimilarity="precomputed", random_state=seed, n_jobs=1,
-    #                    n_init=1)
-    #npos = nmds.fit_transform(similarities, init=pos)
-
-    # Rescale the data
-#    if patterns:
-#        pos *= np.sqrt((patterns ** 2).sum()) / np.sqrt((pos ** 2).sum())
-    #npos *= np.sqrt((X_true ** 2).sum()) / np.sqrt((npos ** 2).sum())
+    # rescale
+    #pos *= sqrt((X_true ** 2).sum()) / sqrt((pos ** 2).sum())
 
     # Rotate the data
     clf = PCA(n_components=2)
     pos = clf.fit_transform(pos)
-    #npos = clf.fit_transform(npos)
     return pos
